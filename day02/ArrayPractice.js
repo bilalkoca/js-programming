29a36ffef8e2ea957256f0ad22c7e4be979040fd
bilal <bilal.koca@gmail.com>
2024-11-27 14:53:06 +0300

    day02

---
+++ day02/ArrayPractice.js
@@ -0,0 +1,28 @@
+let elements = ["Java", "JavaScript", "Python", 10, 20, 30, true, false];
+
+console.log(elements);
+console.log("--------------------------------");
+
+let students = ["Irina", "Maryam", "Daniela", "Andrei"];
+console.log(students);
+console.log(students.length);
+students.push("Ilia", "Julia");//add last
+console.log(students);
+
+//students[students.length-3]="Sophy";//change
+console.log(students);
+
+//students.unshift("Ahmed"); //add first
+console.log(students);
+
+//students.splice(1, 2);//remove 1. and 2. element
+//console.log(students);
+
+//students.shift();//remove first
+//students.pop();//remove last  
+//console.log(students);
+
+console.log("--------------------------------");
+for (let i=students.length-1; i>=0; i--){
+    console.log(students[i]);
+}
---
+++ day02/StringPractice.js
@@ -0,0 +1,38 @@
+let schoolName='Cydeo School';
+console.log(typeof schoolName);
+console.log(schoolName.length);
+console.log(schoolName[schoolName.length - 1]);
+console.log(schoolName[0]);
+console.log(schoolName[5]);
+
+console.log("--------------------------------");
+let program="javascript";
+//console.log(program.toUpperCase());
+program=program.toUpperCase();
+console.log(program);
+
+console.log("--------------------------------");
+
+let word1="JavaScript";
+let word2="javascript";
+
+console.log(word1===word2); // false
+console.log(word1.toLowerCase()===word2.toLowerCase()); //true
+
+console.log("--------------------------------");
+
+let s = "ABCDEFGHIJKL";
+console.log(s.includes("J"));
+console.log("--------------------------------");
+
+let x = "Python Python";
+let x2 = x.charAt().replace(x(1),"JavaScript");
+console.log(x2);
+console.log("--------------------------------");
+
+let email = "cydeoschool@hotmail.com";
+
+let domain = email.substring(email.indexOf("@") + 1, email.lastIndexOf("."));
+
+console.log(domain);
+
